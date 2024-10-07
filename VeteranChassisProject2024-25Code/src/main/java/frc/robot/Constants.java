0d8496e33165dd8bad119f9a92d17f87cedc2be1
KyleKan <142550596+KyleKan@users.noreply.github.com>
2024-10-07 16:56:25 -0700

    Added phoenix5 lib and made motors
    Needa add followers and continue code

--- VeteranChassisProject2024-25Code/src/main/java/frc/robot/Constants.java
+++ VeteranChassisProject2024-25Code/src/main/java/frc/robot/Constants.java
@@ -16,4 +16,11 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class DriveTrainConstants {
+    public static final int kFrontRight = 0;
+    public static final int kFrontLeft = 1;
+    public static final int kBackRight = 2;
+    public static final int kBackLeft = 3;
+  }
 }
---
+++ VeteranChassisProject2024-25Code/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  private WPI_TalonSRX FrontRightM; 
+  private WPI_TalonSRX FrontLeftM; 
+  private WPI_TalonSRX BackRightM; 
+  private WPI_TalonSRX BackLeftM; 
+
+  DifferentialDrive DiffDrive = new DifferentialDrive(FrontLeftM, FrontRightM);
+
+
+  /** Creates a new DrivetrainSubsystem. */
+  public DrivetrainSubsystem() {
+    FrontRightM = new WPI_TalonSRX(Constants.DriveTrainConstants.kFrontRight);
+    FrontLeftM = new WPI_TalonSRX(Constants.DriveTrainConstants.kFrontLeft);
+    BackRightM = new WPI_TalonSRX(Constants.DriveTrainConstants.kBackRight);
+    BackLeftM = new WPI_TalonSRX(Constants.DriveTrainConstants.kBackLeft);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
