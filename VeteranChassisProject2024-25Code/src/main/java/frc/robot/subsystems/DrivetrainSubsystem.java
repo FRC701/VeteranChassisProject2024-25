c8ad7de4c5d8d84df50f57917f0b4948ed9031e7
Kyle Kang <kylek@vandenrobotics.com>
2024-10-28 16:52:54 -0700

    Made followers
    example code: https://github.com/CrossTheRoadElec/Phoenix5-Examples/blob/master/Java%20General/DifferentialDrive/src/main/java/frc/robot/Robot.java

--- VeteranChassisProject2024-25Code/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ VeteranChassisProject2024-25Code/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix6.controls.Follower;
 
 public class DrivetrainSubsystem extends SubsystemBase {
   private WPI_TalonSRX FrontRightM; 
@@ -25,8 +26,13 @@ public class DrivetrainSubsystem extends SubsystemBase {
     FrontLeftM = new WPI_TalonSRX(Constants.DriveTrainConstants.kFrontLeft);
     BackRightM = new WPI_TalonSRX(Constants.DriveTrainConstants.kBackRight);
     BackLeftM = new WPI_TalonSRX(Constants.DriveTrainConstants.kBackLeft);
-
+   
+    BackLeftM.follow(FrontLeftM);
+    BackRightM.follow(FrontRightM);
   }
+  
+    
+
 
   @Override
   public void periodic() {
